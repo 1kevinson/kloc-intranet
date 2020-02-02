8f3887c62392cf6575bece12a305b132eea9ceb3
Arsene Kevin <kouomeukevin@gmail.com>
2020-02-02 23:36:58 +0100

    project set up

---
+++ src/Controller/homepage/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controller\homepage;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home_page")
+     */
+    public function index()
+    {
+        return $this->render('homepage/homepage.twig');
+    }
+}
