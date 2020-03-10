a47bf222327d9e2b90bc238e4e23cbfd23323f7c
Arsene Kevin <kouomeukevin@gmail.com>
2020-03-10 04:47:44 +0100

    design tenant page + responsive flex

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,12 @@
 /* Footer date for copyright */
+/*
 let date = new Date();
 year = date.getFullYear().toString();
 
 
 y = document.getElementById('footer-year');
 y.innerText = year;
+*/
 
 
 /* Change password to text */
